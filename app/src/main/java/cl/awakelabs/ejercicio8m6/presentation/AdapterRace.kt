8286b0ebb7e7beda64ce761c104858deaf1c4d96
Alex Muñoz <lomejor77@gmail.com>
2023-08-09 19:23:23 -0400

    Ejercicio 8 modulo 6 se implementa fragmento detalle y se agrega la navegacion desde el fragmento principal

--- app/src/main/java/cl/awakelabs/ejercicio8m6/presentation/AdapterRace.kt
+++ app/src/main/java/cl/awakelabs/ejercicio8m6/presentation/AdapterRace.kt
@@ -2,8 +2,10 @@ package cl.awakelabs.ejercicio8m6.presentation
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import cl.awakelabs.ejercicio4mod6.R
 import cl.awakelabs.ejercicio4mod6.databinding.ActivityMainBinding
 import cl.awakelabs.ejercicio4mod6.databinding.FragmentRaceBinding
 import cl.awakelabs.ejercicio4mod6.databinding.ItemLayoutBinding
@@ -38,6 +40,9 @@ class AdapterRace: RecyclerView.Adapter<AdapterRace.ItemRaceViewHolder>() {
     class ItemRaceViewHolder(val raceView: ItemLayoutBinding): RecyclerView.ViewHolder(raceView.root) {
         fun bind(race: RaceEntity ){
            raceView.txRace.text = race.race
+           raceView.cardList.setOnClickListener {
+                Navigation.findNavController(raceView.root).navigate(R.id.action_raceFragment_to_detailFragment)
+            }
         }
 
     }
